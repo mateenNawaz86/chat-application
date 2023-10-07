551a2e9a01186f266d64f7954bcad4808f20f818
Mateen Mirani <mateenmirani125@gmail.com>
2023-10-07 09:25:20 +0500

    sign in route added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,9 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const asyncHandler = require("express-async-handler");
 
+// controller for the register the user
 exports.register = asyncHandler(async (req, res) => {
   const { name, email, password, profile } = req.body;
 
@@ -19,11 +22,15 @@ exports.register = asyncHandler(async (req, res) => {
         .json({ error: "User already exit with this email address." });
     }
 
+    // Create a password hash and add salt to the password
+    const salt = await bcrypt.genSalt(10);
+    const securePsw = await bcrypt.hash(password, salt);
+
     // Create a new user here
     const user = await User.create({
       name: name,
       email: email,
-      password: password,
+      password: securePsw,
       profile: profile,
     });
 
@@ -34,6 +41,42 @@ exports.register = asyncHandler(async (req, res) => {
     } else {
       res.status(400).json({ error: "Failed to create the user!" });
     }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+// Controller for login the user
+exports.signIn = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find the entered email from the datase
+    const user = await User.findOne({ email });
+
+    // IF user not exist
+    if (!user) {
+      return res
+        .status(400)
+        .json({ error: "User NOT exit with this email address!" });
+    }
+
+    // Compare the enter password with exist password
+    const comparePsw = await bcrypt.compare(password, user.password);
+
+    // Return Error IF entered Password wrong
+    if (!comparePsw) {
+      return res.status(400).json({ error: "Invalid email or password" });
+    }
+
+    // Return JWT_TOKEN to user as a response
+    const data = {
+      id: user._id,
+    };
+
+    const token = jwt.sign(data, process.env.JWT_SECRET);
+    res.status(201).json({ token, user });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Server error" });
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-// API request for the login
+// API request for the register
 router.post("/register", userController.register);
+
+// API request for the login
+router.get("/sign-in", userController.signIn);
 
 module.exports = router;
